0364122cb5d13ef7bf63665446361e2f28cdb833
faeze <45177517+faezehbagheri@users.noreply.github.com>
2023-07-10 22:26:34 +0330

    get trending gifs

--- app/src/main/java/com/example/giphyapplication/data/remote/GifsService.kt
+++ app/src/main/java/com/example/giphyapplication/data/remote/GifsService.kt
@@ -1,4 +1,20 @@
 package com.example.giphyapplication.data.remote
 
+import com.example.giphyapplication.BuildConfig
+import com.example.giphyapplication.data.entity.GifEntity
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-interface GifsService {}
+
+interface GifsService {
+    @GET("gifs/trending")
+    suspend fun getTrendingGifs(
+        @Query("limit")
+        limit: Int,
+        @Query("offset")
+        offset: Int,
+        @Query("api_key")
+        apiKey: String = BuildConfig.API_KEY
+    ): List<GifEntity>
+
+}
