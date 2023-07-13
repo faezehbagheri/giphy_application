aa5c266bb91d091dd1b36072666853e0a526581e
faeze <45177517+faezehbagheri@users.noreply.github.com>
2023-07-13 11:15:01 +0330

    Add gif list UI

--- app/src/main/java/com/example/giphyapplication/ui/features/gifslist/GifsListScreen.kt
+++ app/src/main/java/com/example/giphyapplication/ui/features/gifslist/GifsListScreen.kt
@@ -1,12 +1,21 @@
 package com.example.giphyapplication.ui.features.gifslist
 
+import FlowRow
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
 import com.example.giphyapplication.ui.features.gifslist.contract.GifsListEvent
 import com.example.giphyapplication.ui.features.gifslist.contract.GifsListIntent
 import com.example.giphyapplication.ui.features.gifslist.contract.GifsListUiState
@@ -57,6 +66,30 @@ fun GifsListScreen(
     onIntent: (GifsListIntent) -> Unit,
 ) = ScreenConfig(uiState = uiState) {
 
+    val configuration = LocalConfiguration.current
+    val size = (configuration.screenWidthDp / 2)
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()) {
+        item{
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                mainAxisSpacing = 10.dp,
+                crossAxisSpacing = 10.dp
+            ) {
+                uiState.gifsList.forEach {
+                    Image(
+                        painter = rememberAsyncImagePainter(it.images.original.url),
+                        contentDescription = null,
+                        modifier = Modifier.size(size.dp)
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
