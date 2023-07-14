e24c90d25f185413124b414d1af03f0ed93a18ee
faeze <45177517+faezehbagheri@users.noreply.github.com>
2023-07-14 17:09:45 +0330

    some changes

---
+++ app/src/main/java/com/example/giphyapplication/common/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.giphyapplication.common
+
+const val LIMIT = 15
--- app/src/main/java/com/example/giphyapplication/data/repository/GifsRepositoryImpl.kt
+++ app/src/main/java/com/example/giphyapplication/data/repository/GifsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.giphyapplication.data.repository
 
+import com.example.giphyapplication.common.LIMIT
 import com.example.giphyapplication.data.remote.GifsService
 import com.example.giphyapplication.data.utils.networkResult
 import com.example.giphyapplication.domain.repository.GifsRepository
@@ -13,7 +14,7 @@ class GifsRepositoryImpl @Inject constructor(
     override fun getTrendingGifs(page: Int) =
         networkResult(
             {
-                gifsService.getTrendingGifs(limit = 15, offset = page * 15)
+                gifsService.getTrendingGifs(limit = LIMIT, offset = page * LIMIT)
             },
             { data ->
                 data.gifs.map { it.toDomain() }
