187850e9eb82fa6a341d107b960f7c56d4ecb4ce
faeze <45177517+faezehbagheri@users.noreply.github.com>
2023-09-13 23:32:30 +0330

    use stickyHeader for search input

--- features/search/src/main/java/com/example/features/search/GifsListScreen.kt
+++ features/search/src/main/java/com/example/features/search/GifsListScreen.kt
@@ -2,13 +2,12 @@ package com.example.features.search
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -18,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -27,12 +25,14 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.rememberAsyncImagePainter
 import com.example.domain.model.Gif
-import com.example.features.search.components.SearchBar
+import com.example.features.search.components.VerticalGrid
+import com.example.features.search.components.searchBar
 import com.example.features.search.contract.GifsListActions
 import com.example.features.search.contract.GifsListViewState
 import com.example.libraries.designsystem.composable.EmptyStateView
 import com.example.libraries.designsystem.composable.ErrorStateView
 import com.example.libraries.designsystem.composable.LoadingStateView
+import com.example.libraries.designsystem.composable.TopBar
 
 @Composable
 fun GifsListScreen(
@@ -87,51 +87,60 @@ private fun MainContent(
 ) {
     val refreshLoadState = lazyPagingItems?.loadState?.refresh
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        SearchBar(searchText = state.searchTerms, onSearch = onSearch)
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        topBar()
+        searchBar(searchText = state.searchTerms, onSearch = onSearch)
         lazyPagingItems?.let {
-            when  {
-                refreshLoadState is LoadState.Loading -> {
-                    LoadingStateView()
-                }
-
-                refreshLoadState is LoadState.Error -> {
-                    ErrorStateView(
-                        onRetry = { lazyPagingItems.retry() },
-                    )
-                }
-
-                lazyPagingItems.itemCount == 0 -> {
-                    EmptyStateView()
-                }
-
-                else -> {
-                    GifsList(
-                        modifier = modifier,
-                        lazyPagingItems = lazyPagingItems,
-                        onNavigateToGifsDetails = onNavigateToGifsDetails
-                    )
-                }
+            when {
+                refreshLoadState is LoadState.Loading -> loading()
+                refreshLoadState is LoadState.Error -> error { lazyPagingItems.retry() }
+                lazyPagingItems.itemCount == 0 -> empty()
+                else -> gifsList(it, onNavigateToGifsDetails)
             }
         }
     }
 }
 
-@Composable
-private fun GifsList(
-    modifier: Modifier = Modifier,
+private fun LazyListScope.topBar() {
+    item { TopBar(withBackButton = false) }
+}
+
+private fun LazyListScope.loading() {
+    item {
+        Box(modifier = Modifier.fillParentMaxSize()) {
+            LoadingStateView()
+        }
+    }
+}
+
+private fun LazyListScope.error(retry: () -> Unit) {
+    item {
+        Box(modifier = Modifier.fillParentMaxSize()) {
+            ErrorStateView(onRetry = { retry() })
+        }
+    }
+}
+
+private fun LazyListScope.empty() {
+    item {
+        Box(modifier = Modifier.fillParentMaxSize()) {
+            EmptyStateView()
+        }
+    }
+}
+
+private fun LazyListScope.gifsList(
     lazyPagingItems: LazyPagingItems<Gif>,
-    onNavigateToGifsDetails: (String) -> Unit
+    onNavigateToGifsDetails: (String) -> Unit,
 ) {
-    LazyVerticalGrid(
-        modifier = modifier.testTag("gifsList"),
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.grid_space)),
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.grid_space))
-    ) {
-        items(count = lazyPagingItems.itemCount) { index ->
-            val gif = lazyPagingItems[index]
-            gif?.let {
+    item {
+        VerticalGrid(
+            columns = 2
+        ) {
+            lazyPagingItems.itemSnapshotList.items.forEach {
                 GifItem(it, onNavigateToGifsDetails)
             }
         }
--- features/search/src/main/java/com/example/features/search/components/SearchBar.kt
+++ features/search/src/main/java/com/example/features/search/components/SearchBar.kt
@@ -1,15 +1,17 @@
 package com.example.features.search.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Icon
@@ -29,81 +31,90 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.features.search.R
 
-
-@Composable
-fun SearchBar(
+@OptIn(ExperimentalFoundationApi::class)
+fun LazyListScope.searchBar(
     searchText: String,
     onSearch: (String) -> Unit,
 ) {
-    Row(
-        modifier = Modifier
-            .padding(
-                horizontal = dimensionResource(id = R.dimen.margin_small),
-                vertical = dimensionResource(id = R.dimen.margin_normal),
-            )
-            .height(44.dp)
-            .fillMaxWidth()
-            .background(Color.White, shape = RoundedCornerShape(8.dp)),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        val interactionSource = remember { MutableInteractionSource() }
-
+    stickyHeader {
         Box(
             modifier = Modifier
-                .weight(1f)
-        ) {
-            BasicTextField(
-                value = searchText,
-                onValueChange = { newText: String ->
-                    onSearch(newText)
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(44.dp)
-                    .padding(dimensionResource(id = R.dimen.margin10)),
-                textStyle = MaterialTheme.typography.body1.copy(
-                    color = Color.Black,
-                ),
-                cursorBrush = Brush.verticalGradient(
-                    0f to MaterialTheme.colors.primary,
-                    1f to MaterialTheme.colors.primary
-                ),
-                singleLine = true,
-                interactionSource = interactionSource,
-            )
-            if (searchText.isEmpty()) {
-                Text(
-                    text = "Search all the GIFs", style = MaterialTheme.typography.body1.copy(
-                        color = Color.LightGray,
-                    ),
-                    modifier = Modifier
-                        .padding(dimensionResource(id = R.dimen.margin10))
+                .background(colorResource(id = R.color.background))
+                .padding(
+                    vertical = dimensionResource(id = R.dimen.margin_normal),
                 )
-            }
-        }
-        Box(
-            modifier = Modifier
-                .height(44.dp)
-                .width(50.dp)
-                .background(
-                    brush = Brush.linearGradient(
-                        colors = listOf(
-                            colorResource(id = R.color.pink),
-                            colorResource(id = R.color.purple),
-                            colorResource(id = R.color.violet),
-                        ),
-                        start = Offset(Float.POSITIVE_INFINITY, 0f),
-                        end = Offset(0f, Float.POSITIVE_INFINITY)
-                    ),
-                    shape = RoundedCornerShape(8.dp)
-                ),
-            contentAlignment = Alignment.Center
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_search),
-                contentDescription = null,
-                tint = Color.White,
-            )
+            Row(
+                modifier = Modifier
+                    .padding(
+                        horizontal = dimensionResource(id = R.dimen.margin_small),
+                    )
+                    .height(44.dp)
+                    .fillMaxWidth()
+                    .background(Color.White, shape = RoundedCornerShape(8.dp)),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                val interactionSource = remember { MutableInteractionSource() }
+
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    BasicTextField(
+                        value = searchText,
+                        onValueChange = { newText: String ->
+                            onSearch(newText)
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(44.dp)
+                            .padding(dimensionResource(id = R.dimen.margin10)),
+                        textStyle = MaterialTheme.typography.body1.copy(
+                            color = Color.Black,
+                        ),
+                        cursorBrush = Brush.verticalGradient(
+                            0f to MaterialTheme.colors.primary,
+                            1f to MaterialTheme.colors.primary
+                        ),
+                        singleLine = true,
+                        interactionSource = interactionSource,
+                    )
+                    if (searchText.isEmpty()) {
+                        Text(
+                            text = "Search all the GIFs",
+                            style = MaterialTheme.typography.body1.copy(
+                                color = Color.LightGray,
+                            ),
+                            modifier = Modifier
+                                .padding(dimensionResource(id = R.dimen.margin10))
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .height(44.dp)
+                        .width(50.dp)
+                        .background(
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    colorResource(id = R.color.pink),
+                                    colorResource(id = R.color.purple),
+                                    colorResource(id = R.color.violet),
+                                ),
+                                start = Offset(Float.POSITIVE_INFINITY, 0f),
+                                end = Offset(0f, Float.POSITIVE_INFINITY)
+                            ),
+                            shape = RoundedCornerShape(8.dp)
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_search),
+                        contentDescription = null,
+                        tint = Color.White,
+                    )
+                }
+            }
         }
     }
 }
@@ -111,10 +122,10 @@ fun SearchBar(
 @Preview
 @Composable
 private fun SearchBarPreview() {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
     ) {
-        SearchBar(searchText = "", onSearch = {})
+        searchBar(searchText = "", onSearch = {})
     }
 }
